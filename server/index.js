00fea5f50fb19d0475c12b43990b2c130dfab68e
jivan9822 <j.toshniwal@yahoo.co.in>
2023-05-11 18:14:02 +0530

    cors header set

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(multer().any());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(
   cors({
     origin: ['http://localhost:5173', 'https://stockinfoapp.netlify.app'],
